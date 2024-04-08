033cd74a988979d3ab8e91bc768b2655e149a21c
Tadashi Saito <tad.a.digger@gmail.com>
2024-04-09 02:18:52 +0900

    Add dummy impl.

--- gnome-wayland-fix-gdk-window.c
+++ gnome-wayland-fix-gdk-window.c
@@ -1,3 +1,5 @@
+#include <stdbool.h>
+
 #define _GNU_SOURCE
 #include <dlfcn.h>
 
@@ -11,8 +13,22 @@ static void ctor(void)
     orig_raise = dlsym(RTLD_NEXT, "gdk_window_raise");
 }
 
+static bool is_managable(GdkWindow *window)
+{
+    window = window;
+    return false;
+}
+
+static void my_raise(GdkWindow *window)
+{
+    orig_raise(window);
+}
+
 void gdk_window_raise(GdkWindow *window)
 {
     fprintf(stderr, "raised!\n");
-    orig_raise(window);
+    if (is_managable(window))
+        my_raise(window);
+    else
+        orig_raise(window);
 }
