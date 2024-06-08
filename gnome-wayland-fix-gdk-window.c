c942fe27428665c814a4dc48d8dfb08ece8a3fbf
Tadashi Saito <tad.a.digger@gmail.com>
2024-06-08 23:59:05 +0900

    Add (silly) assert.

--- gnome-wayland-fix-gdk-window.c
+++ gnome-wayland-fix-gdk-window.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdbool.h>
 #include <stdlib.h>
 
@@ -15,13 +16,12 @@ __attribute__ ((constructor))
 static void ctor(void)
 {
     orig_raise = dlsym(RTLD_NEXT, "gdk_window_raise");
+    assert(orig_raise && "only GTK3 app is supported currently.");
 }
 
 static bool is_managable(GdkWindow *window)
 {
-    return orig_raise != NULL &&
-        window != NULL &&
-        GDK_IS_WAYLAND_WINDOW(window);
+    return window != NULL && GDK_IS_WAYLAND_WINDOW(window);
 }
 
 static MetaWindow *gdkwin_to_metawin(GdkWindow *gwindow)
