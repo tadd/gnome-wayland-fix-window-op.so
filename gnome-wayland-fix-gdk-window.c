ac9f59f91c006882c6005e4cc516921f0cddf13f
Tadashi Saito <tad.a.digger@gmail.com>
2024-06-17 23:36:23 +0900

    Yeah, begin to work!

--- gnome-wayland-fix-gdk-window.c
+++ gnome-wayland-fix-gdk-window.c
@@ -12,12 +12,30 @@
 #include <gdk/gdkwayland.h>
 
 static void (*orig_raise)(GdkWindow *window);
+static GDBusProxy *proxy;
 
 __attribute__ ((constructor))
 static void ctor(void)
 {
     orig_raise = dlsym(RTLD_NEXT, "gdk_window_raise");
     assert(orig_raise && "only GTK3 app is supported currently.");
+    g_autoptr(GError) error = NULL;
+    proxy = g_dbus_proxy_new_for_bus_sync (G_BUS_TYPE_SESSION,
+                                          G_DBUS_PROXY_FLAGS_NONE,
+                                          NULL,
+                                          "org.gnome.Shell",
+                                          "/de/lucaswerkmeister/ActivateWindowByTitle",
+                                          "de.lucaswerkmeister.ActivateWindowByTitle",
+                                          NULL,
+                                          &error);
+
+    assert(!error && "got error");
+}
+
+__attribute__ ((destructor))
+static void dtor(void)
+{
+    g_object_ref(proxy);
 }
 
 static bool is_managable(GdkWindow *window)
@@ -52,7 +70,19 @@ static const char *gdkwin_get_title(GdkWindow *gdkw)
 
 static void activate_by_title(const char *title)
 {
-    title = title;
+    g_autoptr(GError) error = NULL;
+    g_autoptr(GVariant) ret = g_dbus_proxy_call_sync(proxy,
+                                                     "activateByTitle",
+                                                     g_variant_new("(s)", title),
+                                                     G_DBUS_CALL_FLAGS_NONE,
+                                                     -1,
+                                                     NULL,
+                                                     &error);
+#ifdef DEBUG
+    g_autoptr(GVariant) found = g_variant_get_child_value(ret, 0);
+    fprintf(stderr, "%s: %s\n",
+            g_variant_get_boolean(found) ? "found!" : "not found...", title);
+#endif
 }
 
 static void my_raise(GdkWindow *window)
@@ -67,5 +97,6 @@ void gdk_window_raise(GdkWindow *window)
 {
     if (is_managable(window))
         my_raise(window);
-    (*orig_raise)(window);
+    else
+        (*orig_raise)(window);
 }
